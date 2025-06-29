8d97d2e216ebd06968c7ca6f5069127d94e0ca1d
MichalHerda <michalherda83@gmail.com>
2025-06-29 08:59:15 +0200

    sapermodel.h - continue implementation

--- src/sapermodel.cpp
+++ src/sapermodel.cpp
@@ -1,6 +1,42 @@
 #include "sapermodel.h"
 
-SaperModel::SaperModel()
+SaperModel::SaperModel(QObject *parent)
+    : QAbstractTableModel(parent)
 {
+    m_grid.resize(m_rows, QList<CellData>(m_cols));
+}
 
+int SaperModel::rowCount(const QModelIndex &) const {
+    return m_rows;
+}
+
+int SaperModel::columnCount(const QModelIndex &) const {
+    return m_cols;
+}
+
+QVariant SaperModel::data(const QModelIndex &index, int role) const {
+    if (!index.isValid()) return QVariant();
+    const CellData &cell = m_grid[index.row()][index.column()];
+
+    switch (role) {
+    case IsRevealedRole:
+        return cell.isRevealed;
+    case IsFlaggedRole:
+        return cell.isFlagged;
+    case IsMineRole:
+        return cell.isMine;
+    case NeighborMinesRole:
+        return cell.neighborMines;
+    default:
+        return QVariant();
+    }
+}
+
+QHash<int, QByteArray> SaperModel::roleNames() const {
+    QHash<int, QByteArray> roles;
+    roles[IsRevealedRole] = "isRevealed";
+    roles[IsFlaggedRole] = "isFlagged";
+    roles[IsMineRole] = "isMine";
+    roles[NeighborMinesRole] = "neighborMines";
+    return roles;
 }
--- src/sapermodel.h
+++ src/sapermodel.h
@@ -5,6 +5,13 @@
 #include <QStandardItemModel>
 #include <QMetaEnum>
 
+struct CellData {
+    bool isRevealed = false;
+    bool isFlagged = false;
+    bool isMine = false;
+    int neighborMines = 0;
+};
+
 class SaperModel : public QAbstractTableModel
 {
     Q_OBJECT
@@ -17,9 +24,18 @@ public:
     };
     Q_ENUM(SaperRoles)
 
-    explicit SaperModel();
+    explicit SaperModel(QObject *parent = nullptr);
 
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    QHash<int, QByteArray> roleNames() const override;
 signals:
+
+private:
+    int m_rows = 10;
+    int m_cols = 10;
+    QList<QList<CellData>> m_grid;
 };
 
 #endif // SAPERMODEL_H
